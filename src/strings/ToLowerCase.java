425be08706c22b05c59338690e9c19e0e5aaa4f9
OmAximani0 <aximaniom@gmail.com>
2022-04-19 11:48:38 +0530

    Added solution for 'To lower case' question

---
+++ src/strings/ToLowerCase.java
@@ -0,0 +1,18 @@
+package strings;
+
+public class ToLowerCase {
+	public static void main(String[] args) {
+		String s = "LOVELY";
+		System.out.println(toLowerCase(s));
+	}
+	
+	public static String toLowerCase(String s) {
+		for (int i = 0; i < s.length(); i++) {
+			
+			if((int)s.charAt(i) <= 90) {
+				s = s.replace(s.charAt(i),(char) (s.charAt(i)+32));
+			}
+		}
+        return s;
+    }
+}
